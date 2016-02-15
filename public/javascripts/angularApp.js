a68879a947088a9a3e4fe9dde5d5c8d9f714366a
Bethany Nicolle Griggs <bethanygriggs@outlook.com>
2016-02-15 09:07:10 +0000

    I got mongo working!

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -7,25 +7,69 @@ app.config([
   function($stateProvider, $urlRouterProvider) {
     // name: home, url = /home, templateurl = /home.html
     $stateProvider
-       .state('home', {
-         url: '/home',
-         templateUrl: '/home.html',
-         controller: 'MainCtrl'
-       })
-       .state('studies/create', {
-         url: '/studies/create',
-         templateUrl: '/studies/create.html',
-         controller: 'MainCtrl'
-       });
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('studies/create', {
+        url: '/studies/create',
+        templateUrl: '/studies/create.html',
+        controller: 'StudyCtrl',
+        resolve: {
+          postPromise: ['studies', function(posts) {
+            return posts.getAll();
+          }]
+        }
+      });
 
     $urlRouterProvider.otherwise('home');
   }
 ]);
 
 
-// controllers
-app.controller('MainCtrl', [
-'$scope',
-function($scope){
+// services
+app.factory('studies', ['$http', function($http) {
 
-}]);
+      var o = {
+        studies: []
+      };
+      o.getAll = function() {
+        return $http.get('/api/studies').success(function(data) {
+          angular.copy(data, o.studies);
+        });
+      };
+        o.create = function(study) {
+          return $http.post('/api/studies', study).success(function(data) {
+            o.studies.push(data);
+          });
+        };
+        return o;
+      }]);
+
+
+    // controllers
+    app.controller('MainCtrl', [
+      '$scope',
+      function($scope) {
+
+      }
+    ]);
+
+    // controllers
+    app.controller('StudyCtrl', [
+      '$scope', 'studies',
+      function($scope, studies) {
+        $scope.studies = studies.studies;
+        $scope.addStudy = function() {
+          studies.create({
+            title: $scope.title,
+            link: $scope.link,
+          });
+          alert($scope.studies);
+          $scope.title = '';
+          $scope.author = '';
+          $scope.year = '';
+        };
+      }
+    ]);
